ca7c2074ab361814dc473eea3f51988b8c80a057
juan-gabriel-ruiz <juanruizpoket@gmail.com>
2024-06-07 17:59:06 -0300

    Primer Front para Entrada

--- src/app/componentes/entrada/entrada.component.ts
+++ src/app/componentes/entrada/entrada.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-entrada',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, FormsModule],
   templateUrl: './entrada.component.html',
   styleUrl: './entrada.component.css'
 })
 export class EntradaComponent {
+
+  nombre: string = '';
+  turnoGenerado: boolean = false;
+  nombreGenerado: string = '';
+  categoria: string = '';
+
+ 
+  generarTurno(tipocategoria: string) {
+    if (this.nombre.trim() === '') {
+       alert('Por favor, ingrese su nombre.');
+      return;
+    }
+
+    // Guardar el nombre y tipo de turno en variables locales para mostrar
+    this.nombreGenerado = this.nombre;
+    this.categoria = tipocategoria;
+
+    // Limpiar el campo de nombre
+    this.nombre = '';
+
+    // Mostrar la sección del turno generado
+    this.turnoGenerado = true;
+  }
 
 }
