0e9a0fb06ec0aeb69d1824e7139bb21cfe284b2d
Cinthia M. Orona <devrider2021@gmail.com>
2024-06-07 03:07:53 -0300

    Agregue cambios al componente seguridad

--- src/app/componentes/atencionseguridad/atencionseguridad.component.ts
+++ src/app/componentes/atencionseguridad/atencionseguridad.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { CategoriaDetalleComponent } from '../categoria-detalle/categoria-detalle.component';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-atencionseguridad',
   standalone: true,
-  imports: [HeaderComponent, CategoriaDetalleComponent, NavbarComponent],
+  imports: [
+    HeaderComponent,
+    CategoriaDetalleComponent,
+    NavbarComponent,
+    RouterLink,
+  ],
   templateUrl: './atencionseguridad.component.html',
   styleUrl: './atencionseguridad.component.css',
 })
-export class AtencionseguridadComponent {
+export class AtencionseguridadComponent implements OnInit {
   vistaHeader = true; //para ver el header condicionalmente
+  url: string = '';
+
+  constructor(private route: ActivatedRoute) {
+    this.url = this.route.snapshot.url[0].path;
+    console.log('url', this.url);
+  }
+
+  ngOnInit(): void {}
 }
