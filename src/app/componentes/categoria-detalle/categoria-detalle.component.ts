a11ffea8983c892738cc37ccd343e571ac7f2423
Cinthia M. Orona <devrider2021@gmail.com>
2024-06-07 02:43:24 -0300

    Modifique el servicio completo y el componente categoria-detalle

--- src/app/componentes/categoria-detalle/categoria-detalle.component.ts
+++ src/app/componentes/categoria-detalle/categoria-detalle.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ITurno } from '../../interfaces/ITurno';
+import { TurnoService } from '../../servicios/turno.service';
 
 @Component({
   selector: 'app-categoria-detalle',
@@ -11,31 +13,73 @@ import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 })
 export class CategoriaDetalleComponent implements OnInit {
   categoria: string = '';
-  turnos: any[] = [];
+  id: string = '';
+  url: string = '';
 
-  constructor(private route: ActivatedRoute) {}
+  turnos: any[] = [];
+  turnosFiltrados: ITurno[] = [];
+  turnoSeleccionado: any = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private turnoService: TurnoService,
+    private router: Router
+  ) {
+    this.url = this.route.snapshot.url[0].path;
+    console.log('url', this.url);
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.categoria = params['nombre'];
-      this.loadTurnos(this.categoria);
+      this.id = params['id'];
+      console.log('categoria', this.categoria);
+      // if (this.categoria) {
+      //   this.verTurnosPorCategoria(this.categoria);
+      // }
+      // if (this.id) {
+      //   this.verTurnoDetalle(this.id);
+      // }
+      this.loadTurnos();
     });
   }
 
-  //Codigo de muestra hasta cargar los servicios del backend
-  loadTurnos(categoria: string): void {    
-    if (categoria === 'perros') {
-      this.turnos = [25, 26, 27, 28];
-    } else if (categoria === 'gatos') {
-      this.turnos = [3, 4, 5];
-    }else if (categoria === 'animales-exoticos') {
-      this.turnos = [10, 11, 12];
-    }else if (categoria === 'vacunacion') {
-      this.turnos = [15, 16, 17];
-    }else if (categoria === 'compras') {
-      this.turnos = [20, 21, 22];
-    }else{
-      this.turnos = [];
+  loadTurnos(): void {
+    this.turnoService.getData().subscribe((data: ITurno[]) => {
+      try {
+        this.turnos = data;
+        if (this.categoria) {
+          this.verTurnosPorCategoria(this.categoria);
+        }
+        if (this.id) {
+          this.verTurnoDetalle(this.id);
+        }
+      } catch (error) {
+        console.log('Error al cargar los turnos:', error);
+      }
+    });
+  }
+
+  verTurnosPorCategoria(categoria: string) {
+    try {
+      this.turnosFiltrados = this.turnos.filter(
+        (turno) => turno.categoria === categoria
+      );
+    } catch (error) {
+      console.log('Error al filtrar los turnos por categoria:', error);
     }
+  }
+
+  verTurnoDetalle(id: string): void {
+    try {
+      this.turnoSeleccionado = this.turnos.find((turno) => turno._id === id);
+      console.log(this.turnoSeleccionado);
+    } catch (error) {
+      console.log('Error al seleccionar el turno:', error);
+    }
+  }
+
+  trackById(index: number, item: ITurno): string {
+    return item._id;
   }
 }
--- src/app/servicios/turno.service.ts
+++ src/app/servicios/turno.service.ts
@@ -1,85 +1,17 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ITurno } from '../interfaces/ITurno';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TurnoService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient){}
-  private apiUrl: string = 'http://127.0.0.1';
+  private apiTurnos = 'http://localhost:3000/turnos';
 
- 
-  // POST
-
-  //crear un turno
-  //TODO: cambiar any por object o string
-  crearTurno(nombre: string, categoria: string): Observable<any>{
-    const param = new HttpParams()
-    .set('nombre', nombre)
-    .set('categoria', categoria);    
-    return this.http.post<any>(`${this.apiUrl}/turno/crear/${nombre}/${categoria}`, param);
+  getData(): Observable<ITurno[]> {
+    return this.http.get<ITurno[]>(`${this.apiTurnos}`);
   }
-
-  // GET
-
-  //obtener TODOS los turnos de TODAS las categorias
-  //TODO: cambiar el <any> por ITurno
-  traerTodosLosTurnos():Observable<any[]>{
-    return this.http.get<any[]>(`${this.apiUrl}/turno/proximos/`);
-  }
-
-  //obtener TODOS los turnos de UNA categoria especifica
-  //TODO: cambiar el <any[]> por ITurno[]
-  traerTurnosCategoriaEspecifica(categoria: string): Observable<any[]>{
-    return this.http.get<any[]>(`${this.apiUrl}/turno/proximos/${categoria}`);
-  }
-  
-  //obtener UN turno especifico de una categoria
-  //TODO: cambiar string por ICategoria y number por ITurno
-  traerTurnoID(categoria: string, turno: number): Observable<any>{
-    return this.http.get<any>(`${this.apiUrl}/turno/proximo/${categoria}/${turno}`);
-  }
-  
-  //resetear todos los turnos (sin distincion de categoria)
-  //TODO: cambiar el <any> por ITurno
-  resetTurnos(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/turnos/limpiar`);
-  }
-
-  //limpiar todos los turnos segun categoria
-  //TODO: cambiar el <any> por ICategoria
-  limpiarTurnosPorCategoria(categoria: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/turnos/limpiar/${categoria}`);
-  }
-
-  
-  // PUT
-
-  //cambiar estado ATENDIENDO de un turno en una categoria
-  cambiarEstadoAtendiendo(categoria: string, turno: number):Observable<any>{
-    const param = new HttpParams()
-    .set('categoria', categoria)
-    .set('turno', turno);
-    return this.http.put<any>(`${this.apiUrl}/turno/atendiendo/${categoria}/numero=${turno}`, param);
-  }
-
-  //cambiar estado ATENDIDO de un turno en una categoria
-  cambiarEstadoAtendido(categoria: string, turno: number): Observable<any> {
-    const param = new HttpParams()
-      .set('categoria', categoria)
-      .set('turno', turno);
-    return this.http.put<any>(`${this.apiUrl}/turno/atendido/${categoria}/numero=${turno}`, param);
-  }
-
-  //cambiar estado CANCELAR de un turno en una categoria
-  cambiarEstadoCancelar(categoria: string, turno: number): Observable<any> {
-    const param = new HttpParams()
-      .set('categoria', categoria)
-      .set('turno', turno);
-    return this.http.put<any>(`${this.apiUrl}/turno/atendido/${categoria}/numero=${turno}`, param);
-  }
-
-  
 }
