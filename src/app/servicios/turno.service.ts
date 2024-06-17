e41a785a08b74f505ef08eb95e0caebb28b7f8e2
Cinthia M. Orona <devrider2021@gmail.com>
2024-06-17 15:06:04 -0300

    deshaciendo un commit porque crashea

--- src/app/servicios/turno.service.ts
+++ src/app/servicios/turno.service.ts
@@ -9,9 +9,42 @@ import { ITurno } from '../interfaces/ITurno';
 export class TurnoService {
   constructor(private http: HttpClient) {}
 
-  private apiTurnos = 'http://localhost:3000/turnos';
+  // private apiTurnos = 'http://localhost:3000/turnos';
+  private turnero = 'http://localhost:3000/api/turneroweb';
 
   getData(): Observable<ITurno[]> {
-    return this.http.get<ITurno[]>(`${this.apiTurnos}`);
+    return this.http.get<ITurno[]>(`${this.turnero}`);
   }
+
+  crearTurno(turno: ITurno): Observable<ITurno> {
+    console.log('Turno:', turno);
+    return this.http.post<ITurno>(`${this.turnero}`, turno, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+/*
+  getDataById(id: number): Observable<ITurno> {
+    return this.http.get<ITurno>(`${this.turnero}/${id}`);
+  }
+
+  postData(turno: ITurno): Observable<ITurno> {
+    return this.http.post<ITurno>(`${this.turnero}`, turno, {});
+  }
+
+  deleteData(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.turnero}/${id}`);
+  }
+
+  updateData(turno: ITurno): Observable<string> {
+    return this.http.put(`${this.turnero}/${turno._id}`, turno, {
+      responseType: 'text',
+    });
+  }
+
+  getByCategory(category: string): Observable<ITurno[]> {
+    return this.http.get<ITurno[]>(`${this.turnero}/categoria/${category}`);
+  }
+    */
 }
